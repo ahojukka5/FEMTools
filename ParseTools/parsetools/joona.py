81a33314b1d838bd3631250ccc6790d698a8618e
Joona <joonavaa@paju.oulu.fi>
2012-03-22 20:02:58 +0200

    testi

---
+++ ParseTools/parsetools/joona.py
@@ -0,0 +1,16 @@
+"""
+
+Joona module
+
+"""
+
+__all__ = ['fuu']
+
+print __name__
+
+def fuu():
+    print "I'm not!"
+
+if __name__ == '__main__':
+    fuu()
+
--- ParseTools/parsetools/jukka.py
+++ ParseTools/parsetools/jukka.py
@@ -7,7 +7,8 @@ Jukka module
 __all__ = ['foo']
 
 def foo():
-    print "I'm working!"
+    print "I'm working!", "1+1 = ",1+1
 
 if __name__ == '__main__':
     foo()
+
